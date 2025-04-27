27fdece871aa4c415b06d6d566afd6bd7bb6b52a
Asha <ashamersanio28@gmail.com>
2025-04-27 20:52:22 +0530

    Advanced Task

---
+++ Frontend_task/Task_1/js/1_LoginPage.js
@@ -0,0 +1,30 @@
+
+function btn()
+{   
+
+    
+      setInterval(function(){
+      // 
+      if (document.body.style.backgroundColor = "Black") 
+       {
+            document.body.style.backgroundColor = 'white';
+           document.getElementById("Container").style.borderColor = 'white';
+       } 
+       // console.log("dfdssdjhdskihfi");
+    },2000);
+
+    
+}
+    
+
+
+
+
+   
+   
+
+
+
+
+
+
