08fb81147bcd67af039ba6345b6d82523f4fb05c
Asha <ashamersanio28@gmail.com>
2025-04-27 21:08:17 +0530

    Add files via upload

---
+++ Frontend_task/Task_4/js/4_Breadcrumb.js
@@ -0,0 +1,9 @@
+function div_items_open()
+{
+     document.getElementById('subdiv').style.display="block";
+     console.log("hi");
+}
+function div_items_close()
+{
+	document.getElementById('subdiv').style.display="none";
+}
