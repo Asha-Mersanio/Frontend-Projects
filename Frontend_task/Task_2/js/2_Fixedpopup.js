43698019f264d534b8f269ca06f3b7295bbdfd19
Asha <ashamersanio28@gmail.com>
2025-04-27 20:59:20 +0530

    Advanced Task

---
+++ Frontend_task/Task_2/js/2_Fixedpopup.js
@@ -0,0 +1,8 @@
+function remove() {
+
+	document.getElementById('main').style.display="none";
+	document.getElementById('sub').style.display="none";
+	document.getElementById('demo').style.display="none";
+     
+}
+// create instance of kinet with custom settings
